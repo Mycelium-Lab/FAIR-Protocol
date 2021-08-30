ecb9352da477916ea1ddeded7e97fb0f3c379244
knoexus <knoexus@yahoo.com>
2021-08-30 11:07:41 +0300

    rm bignumber in migration

--- fair-token/migrations/2_deploy_contracts.js
+++ fair-token/migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
-const BigNumber = require("bignumber.js")
 const FairToken = artifacts.require("FairToken")
 
 module.exports = (deployer, _, accounts) => {
   const [admin, minter] = accounts
   deployer.then(async () => {
-    const cap = new BigNumber('50000000000000000000000000')
+    const cap = '50000000000000000000000000' // 50 mln. FAIR
     await deployer.deploy(FairToken, 'Fair Token', 'FAIR', cap, admin, minter)
   })
 }
